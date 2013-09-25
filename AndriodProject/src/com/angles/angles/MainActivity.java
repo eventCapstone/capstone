4ac10314412782f96456b383bb09cb1f16da6cd2
MrBrent <Brent.hoch@gmail.com>
2013-09-24 23:17:58 -0500

    ok

--- AndriodProject/src/com/angles/angles/MainActivity.java
+++ AndriodProject/src/com/angles/angles/MainActivity.java
@@ -7,7 +7,7 @@ import com.angles.angles.AnglesController;
 
 @SuppressWarnings("unused")
 public class MainActivity extends Activity {
-	//hello
+	
 
 	private AnglesController  itsMainController = new AnglesController(this);
     @Override
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
     }
 
     
-   public void hello(){
+   public void goodbye(){
 	   
    }
    
