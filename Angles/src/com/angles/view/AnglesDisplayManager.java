ea10d316f2047dac981b967491c8fd10f35fefb8
Puddnhead <michaelwilklow@gmail.com>
2013-11-02 19:10:57 -0600

    Missed a bug in the DisplayManager

--- Angles/src/com/angles/view/AnglesDisplayManager.java
+++ Angles/src/com/angles/view/AnglesDisplayManager.java
@@ -48,7 +48,7 @@ public class AnglesDisplayManager {
 		TextView eventName = (TextView) currentActivity.findViewById(R.id.eventName);
 		eventName.setText("Event Name: " + event.eventTitle);
 		TextView host = (TextView) currentActivity.findViewById(R.id.host);
-		host.setText("Hosted By: " + event.getHost().firstname);
+		host.setText("Hosted By: " + event.getHost().userName);
 		
 		TextView startTime = (TextView) currentActivity.findViewById(R.id.startTime);
 		startTime.setText("Start Time: " + EventsManager.getDisplayDateTime(event.startTime));
