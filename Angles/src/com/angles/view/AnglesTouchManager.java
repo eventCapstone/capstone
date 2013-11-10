98e375890079e4bc4c25ca844b7b8294e1ab5288
nleners <ndleners@gmail.com>
2013-11-10 16:23:10 -0600

    Append - TouchManager

--- Angles/src/com/angles/view/AnglesTouchManager.java
+++ Angles/src/com/angles/view/AnglesTouchManager.java
@@ -304,19 +304,9 @@ public class AnglesTouchManager {
 	 * @param currentActivity The current activity
 	 */
 	public void setOngoingEventListeners(Activity currentActivity) {
-<<<<<<< Updated upstream
 		Button btnCapturePhoto = (Button) currentActivity.findViewById(R.id.btnCapturePhoto);
-		Button btnEventList = (Button) currentActivity.findViewById(R.id.btnEventList);
-		
-		btnCapturePhoto.setOnClickListener(new AnglesClickListener(currentActivity) {
-			public void onClick(View v) {
-				anglesController.loadCameraActivity(currentActivity);
-			}
-		});
-=======
 		
 		Button btnEventList = (Button) currentActivity.findViewById(R.id.btnEventList);
->>>>>>> Stashed changes
 		
 		btnEventList.setOnClickListener(new AnglesClickListener(currentActivity) {
 			public void onClick(View v) {
